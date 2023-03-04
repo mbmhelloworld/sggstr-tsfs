8cb1ab95643fbcb899ab58d0423c895563f71737
mbmhelloworld <71063245+mbmhelloworld@users.noreply.github.com>
2023-03-04 13:45:23 +0100

    Initial commit

---
+++ client/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// Services
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+// Components
+import { CatsComponent } from './components/cats/cats.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AccountComponent } from './components/account/account.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'cats', component: CatsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'account', component: AccountComponent, canActivate: [AuthGuardLogin] },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuardAdmin] },
+  { path: 'notfound', component: NotFoundComponent },
+  { path: '**', redirectTo: '/notfound' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
---
+++ client/app/app.module.ts
@@ -0,0 +1,59 @@
+// Angular
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { JwtModule } from '@auth0/angular-jwt';
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+// Services
+import { CatService } from './services/cat.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+// Components
+import { AppComponent } from './app.component';
+import { CatsComponent } from './components/cats/cats.component';
+import { AddCatFormComponent } from './components/add-cat-form/add-cat-form.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AccountComponent } from './components/account/account.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    AddCatFormComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    SharedModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: (): string | null => localStorage.getItem('token'),
+        // allowedDomains: ['localhost:3000', 'localhost:4200']
+      }
+    })
+  ],
+  providers: [
+    AuthService,
+    AuthGuardLogin,
+    AuthGuardAdmin,
+    CatService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
